75ae7490a5a8cc5ae857cb5ae71c0e63f1102654
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-11 17:02:12 +0300

    Airbnb

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""model for amenity"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """class representing amenity"""
+
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" module for city"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+
+    """class for city"""
+
+    name = ""
+    state_id = ""
