8029cdf7715c8bfb408a9e75fa9212f285ac446b
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-16 18:05:51 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 
@@ -35,7 +35,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a human-readable string representation
@@ -49,7 +49,7 @@ class BaseModel:
         with the current datetime."""
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
