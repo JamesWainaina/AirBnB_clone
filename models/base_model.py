3dbdbb5f5b071901bcf57e59433e24237ef51884
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-16 18:24:44 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,7 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
+        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
