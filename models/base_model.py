69071f11caf1996dd4c502008dbc5af4a0b4e40b
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-16 17:49:20 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage = FileStorage()
             storage.new(self)
 
     def __str__(self):
@@ -50,6 +49,7 @@ class BaseModel:
         with the current datetime."""
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
