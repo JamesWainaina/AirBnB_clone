252f2fddd279ad2be09abaf67a9dcfec07f6425f
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-16 17:33:20 +0300

    the fix

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,9 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.save(self)
+            storage = FileStorage()
+            storage.reload()
+            storage.new(self)
 
     def __str__(self):
         """Returns a human-readable string representation
