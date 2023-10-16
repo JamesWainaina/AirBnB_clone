3bd46fa41fb565fb195ecec8893641115f084c32
JamesWainaina <jamesgatheru9905@gmail.com>
2023-10-16 18:16:06 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,9 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
@@ -32,9 +35,6 @@ class BaseModel:
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
